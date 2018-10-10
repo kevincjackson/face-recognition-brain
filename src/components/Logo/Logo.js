1d15efbf51972832d573c1b67a81495f254c450e
Kevin Jackson <kevin@kevinjacksonart.com>
2018-10-10 16:30:03 -0700

    Adds styled page elements.

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Tilt from "react-tilt";
+import brain from "./brain.png";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2  "
+        options={{ max: 50 }}
+        style={{ height: 250, width: 250 }}
+      >
+        <div className="Tilt-inner">
+          <img src={brain} alt="logo" style={{ paddingTop: "50px" }} />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">{"Kevin, your current rank is ..."}</div>
+      <div className="white f1">{"#12"}</div>
+    </div>
+  );
+};
+
+export default Rank;
