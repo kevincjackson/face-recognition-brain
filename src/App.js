c630a3fa15dcaa8548b17b873a98c69ac9688e87
Kevin Jackson <kevin@kevinjacksonart.com>
2018-10-10 19:49:19 -0700

    Adds face recognition.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navigation from "./components/Navigation/Navigation";
-import Logo from "./components/Logo/Logo";
+import Clarifai from "clarifai";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-import Rank from "./components/Rank/Rank";
+import Logo from "./components/Logo/Logo";
+import Navigation from "./components/Navigation/Navigation";
 import Particles from "react-particles-js";
+import Rank from "./components/Rank/Rank";
 const particlesOptions = {
   particles: {
     number: {
@@ -17,7 +19,49 @@ const particlesOptions = {
   }
 };
 
+// Initialize Clarafai face detection API.
+const app = new Clarifai.App({ apiKey: process.env.REACT_APP_API_KEY });
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {}
+    };
+  }
+
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    this.setState({ box: box });
+  };
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = event => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(resp => this.displayFaceBox(this.calculateFaceLocation(resp)))
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className="App">
@@ -25,10 +69,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {
-          // <FaceRecognition />
-        }
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
       </div>
     );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,12 +7,12 @@ const Logo = () => {
   return (
     <div className="ma4 mt0">
       <Tilt
-        className="Tilt br2 shadow-2  "
-        options={{ max: 50 }}
-        style={{ height: 250, width: 250 }}
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 150, width: 150 }}
       >
-        <div className="Tilt-inner">
-          <img src={brain} alt="logo" style={{ paddingTop: "50px" }} />
+        <div className="Tilt-inner pa3">
+          <img style={{ paddingTop: "5px" }} alt="logo" src={brain} />
         </div>
       </Tilt>
     </div>
