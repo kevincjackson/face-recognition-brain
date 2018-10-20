9edb570c786a4e303d03d06da6bcd0e626bb9e3b
Kevin Jackson <kevin@kevinjacksonart.com>
2018-10-19 23:26:37 -0700

    Refactors front end.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,63 @@
-import React, {Component} from 'react';
-import './App.css';
-import Clarifai from 'clarifai';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Logo from './components/Logo/Logo';
-import Navigation from './components/Navigation/Navigation';
-import Particles from 'react-particles-js';
-import Rank from './components/Rank/Rank';
-import Register from './components/Register/Register';
-import Signin from './components/Signin/Signin';
+import React, { Component } from "react";
+import "./App.css";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Logo from "./components/Logo/Logo";
+import Navigation from "./components/Navigation/Navigation";
+import Particles from "react-particles-js";
+import Rank from "./components/Rank/Rank";
+import Register from "./components/Register/Register";
+import Signin from "./components/Signin/Signin";
 const particlesOptions = {
   particles: {
     number: {
       value: 30,
       density: {
         enable: true,
-        value_area: 800,
-      },
-    },
-  },
+        value_area: 800
+      }
+    }
+  }
 };
 
-// Initialize Clarafai face detection API.
-const app = new Clarifai.App({apiKey: process.env.REACT_APP_API_KEY});
+// Initial state
+const initialState = {
+  input: "",
+  imageUrl: "",
+  box: {},
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: ""
+  }
+};
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: '',
-      },
-    };
+    this.state = initialState;
   }
 
   calculateFaceLocation = data => {
     const clarifaiFace =
       data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
+    const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
       rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height,
+      bottomRow: height - clarifaiFace.bottom_row * height
     };
   };
 
   displayFaceBox = box => {
-    this.setState({box: box});
+    this.setState({ box: box });
   };
 
   loadUser = data => {
@@ -68,47 +67,56 @@ class App extends Component {
         name: data.name,
         email: data.email,
         entries: data.entries,
-        joined: data.joined,
-      },
+        joined: data.joined
+      }
     });
   };
 
   onInputChange = event => {
-    this.setState({input: event.target.value});
+    this.setState({ input: event.target.value });
   };
 
-  onPictureSubmit = event => {
-    this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(resp => {
-        fetch('http://localhost:3000/image', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            id: this.state.user.id,
-          }),
-        })
-          .then(res => res.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, {entries: count}));
-          });
-        this.displayFaceBox(this.calculateFaceLocation(resp));
+  onPictureSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    fetch("http://localhost:3000/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            })
+            .catch(console.log);
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
       .catch(err => console.log(err));
   };
 
   onRouteChange = route => {
-    if (route === 'signout') {
-      this.setState({isSignedIn: false});
-    } else if (route === 'home') {
-      this.setState({isSignedIn: true});
+    if (route === "signout") {
+      this.setState(initialState);
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route});
+    this.setState({ route: route });
   };
 
   render() {
-    const {isSignedIn, imageUrl, route, box} = this.state;
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
@@ -116,7 +124,7 @@ class App extends Component {
           isSignedIn={isSignedIn}
           onRouteChange={this.onRouteChange}
         />
-        {route === 'home' ? (
+        {route === "home" ? (
           <div>
             <Logo />
             <Rank
@@ -129,7 +137,7 @@ class App extends Component {
             />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-        ) : route === 'signin' ? (
+        ) : route === "signin" ? (
           <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
           <Register
