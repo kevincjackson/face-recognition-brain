a17490645c6cad4233afb435214e1c27d91b804d
Kevin Jackson <kevin@kevinjacksonart.com>
2018-10-24 23:29:55 -0700

    Uses production back-end server.

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -22,7 +22,7 @@ class Signin extends React.Component {
     if (!signInEmail || !signInPassword) {
       return;
     }
-    fetch("http://localhost:3000/signin", {
+    fetch("https://secure-citadel-42737.herokuapp.com/signin", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
